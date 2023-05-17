505f3281e27256fd678cb8b8693ed293c6d45cf6
Gustavo Parizzato <gustavoventura246@gmail.com>
2023-05-17 19:52:36 -0300

    Initial commit

---
+++ src/main/java/ProjectExe/Integracao/IntegracaoApplication.java
@@ -0,0 +1,13 @@
+package ProjectExe.Integracao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracaoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ProjectExe/Integracao/entities/Loja.java
@@ -0,0 +1,10 @@
+package ProjectExe.Integracao.entities;
+
+public class Loja {
+
+    private String razaosocial;
+    private String nomefantasia;
+    private String cnpj;
+    private String email;
+    private String telefone;
+}
