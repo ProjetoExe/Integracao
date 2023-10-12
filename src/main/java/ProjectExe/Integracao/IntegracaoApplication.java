7409c52dd5cafa756f40cb6d3fd0c64d0c960582
Gustavo Parizzato <gustavoventura246@gmail.com>
2023-10-12 18:34:30 -0300

    Reversão 'Teste commit'

--- src/main/java/ProjectExe/Integracao/IntegracaoApplication.java
+++ src/main/java/ProjectExe/Integracao/IntegracaoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IntegracaoApplication {
 
-	//teste
 	public static void main(String[] args) {
 		SpringApplication.run(IntegracaoApplication.class, args);
 	}
