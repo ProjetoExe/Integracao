31546c932b69c4e31b7e836064e4d9bf46590d04
Gustavo Parizzato <gustavoventura246@gmail.com>
2023-10-24 20:36:00 -0300

    Removido método findBy da ProdutoRepositorio

--- src/main/java/ProjectExe/Integracao/repositorios/ProdutoRepositorio.java
+++ src/main/java/ProjectExe/Integracao/repositorios/ProdutoRepositorio.java
@@ -1,16 +1,12 @@
 package ProjectExe.Integracao.repositorios;
 
-import ProjectExe.Integracao.dto.ProdutoResumidoDTO;
 import ProjectExe.Integracao.entidades.Produto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ProdutoRepositorio extends JpaRepository<Produto, Long> {
-
-    Page<ProdutoResumidoDTO> findBy(Pageable pageable);
 
     Page<Produto> findByNomeContainingAndAtivo(String nome, char ativo, Pageable pageable);
 
