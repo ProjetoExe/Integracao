e83138dbc75b79bc702998818f6aeca12d793d46
Gustavo Parizzato <gustavoventura246@gmail.com>
2023-12-20 11:58:44 -0300

    Inserido busca por Cpf em ClienteRepositorio. Inserido busca da maior data de pagamento referente a um número de venda em PagamentoRepositorio.

--- src/main/java/ProjectExe/Integracao/repositorios/ClienteRepositorio.java
+++ src/main/java/ProjectExe/Integracao/repositorios/ClienteRepositorio.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 public interface ClienteRepositorio extends JpaRepository<Cliente, Long> {
 
-    @Query
     Optional<Cliente> findByNomeCliente(String nome);
+
+    Optional<Cliente> findByCpf(String cpf);
 }
--- src/main/java/ProjectExe/Integracao/repositorios/PagamentoRepositorio.java
+++ src/main/java/ProjectExe/Integracao/repositorios/PagamentoRepositorio.java
@@ -3,5 +3,10 @@ package ProjectExe.Integracao.repositorios;
 import ProjectExe.Integracao.entidades.Pagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface PagamentoRepositorio extends JpaRepository<Pagamento, Long> {
+
+    Pagamento findFirstByVenda_VendaIdOrderByDataDesc(UUID vendaId);
 }
